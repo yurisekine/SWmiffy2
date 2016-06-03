894f3738f75e5c1a6c7b6e396246667eaa3d8a29
aoiaoiaoi <tsuda.soft.2015+github@gmail.com>
2016-06-03 10:44:11 +0900

    miffy

---
+++ miffy/src/jp/tuyano/DelLinkDataServlet.java
@@ -0,0 +1,33 @@
+package jp.tuyano;
+ 
+import java.io.IOException;
+ 
+import javax.jdo.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+ 
+public class DelLinkDataServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    @Override
+    protected void doGet(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("no url...");
+    }
+ 
+    @Override
+    protected void doPost(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        long id = Long.parseLong(req.getParameter("id"));
+        PersistenceManagerFactory factory = PMF.get();
+        PersistenceManager manager = factory.getPersistenceManager();
+        LinkData data = (LinkData)manager.getObjectById(LinkData.class,id);
+        manager.deletePersistent(data);
+        manager.close();
+        resp.sendRedirect("/index.html");
+    }
+}
